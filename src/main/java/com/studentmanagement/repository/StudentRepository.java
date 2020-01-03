12026cc06fa0648bacebe1842aebd0ba9f4625e4
gun <kevinenzpadi@gmail.com>
2020-01-03 14:07:44 +0100

    login

---
+++ src/main/java/com/studentmanagement/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.studentmanagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.studentmanagement.model.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+}
